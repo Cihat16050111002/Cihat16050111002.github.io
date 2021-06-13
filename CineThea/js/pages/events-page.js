46ebd197736ff178b2cfb7ddf3055003c36b1977
ByUnal <16050111002@ybu.edu.tr>
2021-06-13 16:24:00 +0300

    Swalfire added
    Swal fire is added to Organize event

--- CineThea/js/pages/events-page.js
+++ CineThea/js/pages/events-page.js
@@ -51,23 +51,14 @@
     })
 
 
-    $('.form-group .login_btn').click(() => {
-        if(valid[0].validity.valid && valid[1].validity.valid) {
-            Swal.fire({
-                title: 'Successfull',
-                text: 'Logged In Successfully',
-                icon: 'success',
-                confirmButtonText: 'Confirm'
-            }).then(() => window.location.href="index.html");
-        }
-        else {
-            Swal.fire({
-                title: 'Error!',
-                text: 'Please fill the information correctly',
-                icon: 'error',
-                confirmButtonText: 'Confirm'
-            });
-        }
+    $('#form-submit').click(() => {
+        Swal.fire({
+            title: 'Successfull',
+            text: 'Logged In Successfully',
+            icon: 'success',
+            confirmButtonText: 'Confirm'
+        }).then(() => window.location.reload());
+
     });
 
 }(jQuery));
--- CineThea/js/pages/login-page.js
+++ CineThea/js/pages/login-page.js
@@ -2,14 +2,23 @@ $(document).ready(function () {
     let valid = $("input").jqBootstrapValidation();
 
 
-    $('#form-submit').click(() => {
-        Swal.fire({
-            title: 'Successfull',
-            text: 'Logged In Successfully',
-            icon: 'success',
-            confirmButtonText: 'Confirm'
-        }).then(() => window.location.reload());
-
+    $('.form-group .login_btn').click(() => {
+        if(valid[0].validity.valid && valid[1].validity.valid) {
+            Swal.fire({
+                title: 'Successfull',
+                text: 'Logged In Successfully',
+                icon: 'success',
+                confirmButtonText: 'Confirm'
+            }).then(() => window.location.href="index.html");
+        }
+        else {
+            Swal.fire({
+                title: 'Error!',
+                text: 'Please fill the information correctly',
+                icon: 'error',
+                confirmButtonText: 'Confirm'
+            });
+        }
     });
 });
 
